38840ad87f964ee73061d3632c17e69f73ee3b73
Eman  Al-Shaikh Hussain <97835837+Eman-Alshaikh@users.noreply.github.com>
2022-05-22 08:22:39 +0300

    Delete snack_tracker_project directory

--- snack_tracker_project/things_rater/urls.py
+++
@@ -1,8 +0,0 @@
-from unicodedata import name
-from django.urls import URLPattern, path
-from .views import ThingsListView
-
-urlpatterns ={
-    path('things-list',ThingsListView.as_view(),name='things_list')
-
-}
