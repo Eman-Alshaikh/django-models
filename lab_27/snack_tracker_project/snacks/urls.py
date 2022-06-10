53f25e59e0464213ce6bf62c084bd6b4055f223d
Eman Al-Shaikh Hussain <engemanalsheikh@gmail.com>
2022-06-10 10:48:27 +0300

    lab 27

--- lab_27/snack_tracker_project/snacks/urls.py
+++ lab_27/snack_tracker_project/snacks/urls.py
@@ -4,7 +4,7 @@ from snacks.models import Snack
 from .views import SnacksDetailView, SnacksListView
 
 urlpatterns=[
-path('snacks_list',SnacksListView.as_view(),name="snacks_list"),
+path('',SnacksListView.as_view(),name="snacks_list"),
 path('<int:pk>/',SnacksDetailView.as_view(),name="snack_detail"),
 
 ]
